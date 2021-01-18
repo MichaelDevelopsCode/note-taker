c511fcca4f20a255dd8e375c69fb9e4fe8f3793e
Michael Arvelo <michaeldevelopscode@gmail.com>
2021-01-17 19:25:37 -0500

    added delete requests

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 // import db json
 const { notes } = require('../../db/db.json'); 
 // import functions
-const { createNewNote } = require('../../lib/notes');
+const { createNewNote, deleteNote } = require('../../lib/notes');
 
 const router = require('express').Router();
 
@@ -13,6 +13,7 @@ router.get('/notes', (req,res) => {
     return res.json(notes);
 });
 
+// post to db.json
 router.post('/notes', (req,res) => {
     // generate id
     const uniqueId = uuidv4();
@@ -21,6 +22,21 @@ router.post('/notes', (req,res) => {
     // create note and return it
     const note = createNewNote(req.body, notes);
     return res.json(note);
+});
+
+// delete from db 
+router.delete('/notes/:id', (req,res) => {
+    // var for id searched
+    const searchedId = req.params.id;
+    // get index of object with id searched
+    const response = deleteNote(searchedId, notes);
+
+    // see if id is in notes then delete else send error
+    if (response) {
+        return res.status(200).send(`Note with ID: ${searchedId} deleted from database`)
+    } else {
+        return res.status(400).send("Enter a valid note id to delete");
+    }
 });
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// redirect to main html
 router.get('/', (req, res) => {
   return res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+// redirect to notes html page
 router.get('/notes', (req, res) => {
   return res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
